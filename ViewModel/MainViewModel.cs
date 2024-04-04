7fd5dbb124e8e8160fa08964fc9ecf877a48a464
anastasiialepilina123 <a.lepilina@internet.ru>
2024-04-04 13:58:13 +0300

    ddf

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+using POINT.ViewModel.Helpers;
+
+namespace POINT.ViewModel
+{
+    internal class MainViewModel : BindingHelper
+    {
+        public ICommand Start { get; set; }
+        public MainWindow Window { get; set; }
+        public MainViewModel(MainWindow Window)
+        {
+            Start = new BindableCommand(StartMoment);
+            this.Window = Window;
+        }
+        public void StartMoment(object parameter)
+        {
+            WindowStart windowStart = new WindowStart();
+            windowStart.Show();
+            Window.Close();
+        }
+    }
+}
---
+++ ViewModel/ViewModelSave.cs
@@ -0,0 +1,32 @@
+﻿using DevExpress.Data.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using POINT.ViewModel.Helpers;
+using System.Windows.Interactivity;
+using System.Windows.Input;
+using System.Windows;
+
+namespace POINT.ViewModel
+{
+    internal class ViewModelSave : Helpers.BindingHelper
+    {
+        public ICommand Save { get; set; }
+        public WindowSave Window { get; set; }
+        public ViewModelSave(WindowSave Window) 
+        {
+            Save = new BindableCommand(SaveEnd);
+            this.Window = Window;
+        }
+
+        public void SaveEnd(object parameters)
+        {
+            WindowStart windowStart = new WindowStart();
+            windowStart.Show();
+            Window.Close();
+        }
+
+    }
+}
